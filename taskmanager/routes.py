5aac4bc1a612e5339ce46b67f7536821cd7097ec
Luke Masterson <lukemasterson@Lukes-MacBook-Air.local>
2024-04-21 12:34:58 +0100

    added tables

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
     return render_template("base.html")
+
+# Ensure that the creation of all tables is within the application context
+with app.app_context():
+    # Create all tables
+    db.create_all()
